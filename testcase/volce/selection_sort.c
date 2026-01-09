f42388f16c5d83186733800a575bf12a65ac6b60
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-01-09 11:35:34 +0800

    Create selection_sort.c

---
+++ testcase/volce/selection_sort.c
@@ -0,0 +1,26 @@
+void selectionSort(int arr[10])
+{
+    int i = 0;
+    while (i < 9)
+    {
+        int min_idx = i;
+        int j = i + 1;
+        while (j < 10)
+        {
+            if (arr[j] < arr[min_idx])
+            {
+                min_idx = j;
+            }
+            j = j + 1;
+        }
+
+        if (min_idx != i)
+        {
+            int tmp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = tmp;
+        }
+
+        i = i + 1;
+    }
+}
