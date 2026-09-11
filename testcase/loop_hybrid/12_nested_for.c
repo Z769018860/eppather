78a4c7d8782a357249fb4ef66d59db2daeffa920
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-11 17:22:33 +0800

    fix: make loop integration cases analyzable

--- testcase/loop_hybrid/12_nested_for.c
+++ testcase/loop_hybrid/12_nested_for.c
@@ -1,1 +1,1 @@
-int main(int n) { int a[3][4]; int s = 0; for (int i = 0; i < 3; i = i + 1) { for (int j = 0; j < 4; j = j + 1) { a[i][j] = n + i + j; s = s + a[i][j]; } } return s; }
+int main(int n) { int a[12]; int s = 0; for (int i = 0; i < 3; i = i + 1) { for (int j = 0; j < 4; j = j + 1) { int k = i * 4 + j; a[k] = n + i + j; s = s + a[k]; } } return s; }
