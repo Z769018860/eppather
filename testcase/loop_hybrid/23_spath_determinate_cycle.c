13690b3e577afbeb1bc30eb541110f858ec06213
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-24 01:13:03 +0800

    Use simple constant periodic fixture for end-to-end acceleration

--- testcase/loop_hybrid/23_spath_determinate_cycle.c
+++ testcase/loop_hybrid/23_spath_determinate_cycle.c
@@ -2,10 +2,10 @@ int main() {
     int state = 0;
     int i = 0;
     while (i < 4) {
-        if (state >= 0) {
-            state = 0 - state - 1;
+        if (state < 1) {
+            state = 2;
         } else {
-            state = 0 - state - 1;
+            state = 0;
         }
         i = i + 1;
     }
